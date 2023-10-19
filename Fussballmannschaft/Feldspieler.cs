83cb82c583ffb47ef25d493feb46aef27ada07bd
Carina Cordes <carina.cordes.ch@gmail.com>
2023-10-20 01:58:48 +0200

    Add the name of the 'Feldspieler'

--- Fussballmannschaft/Feldspieler.cs
+++ Fussballmannschaft/Feldspieler.cs
@@ -8,8 +8,18 @@ namespace Fussballmannschaft
 {
     public class Feldspieler
     {
+        public string Name { get; set; }
+
+        public Feldspieler(string name)
+        {
+            Name = name;
+        }
+
+
+
         public bool SchiesseTor()
         {
+            Console.WriteLine($"{Name} schiesst aufs Tor!");
             Random random = new Random();
             return random.Next(100) < 50; //50% chances
         }
