683f22ee38a812df14bdbcd153cd3f29b3764fb6
Carina Cordes <carina.cordes.ch@gmail.com>
2023-10-20 14:15:47 +0200

    initiate two teams

--- Fussballmannschaft/Program.cs
+++ Fussballmannschaft/Program.cs
@@ -4,23 +4,41 @@ class Program
 {
     static void Main()
     {
-        Mannschaft team = new Mannschaft();
+        Mannschaft team1 = new Mannschaft();
+        Mannschaft team2 = new Mannschaft();
 
-        Feldspieler spieler1 = new Feldspieler("Spieler1");
-        Feldspieler spieler2 = new Feldspieler("Spieler2");
-        Torwart torwart = new Torwart("Torwart1");
+        //team 1
+        Feldspieler spieler1Team1 = new Feldspieler("Spieler 1 Team 1");
+        Feldspieler spieler2Team1 = new Feldspieler("Spieler 2 Team 1");
+        Torwart torwartTeam1 = new Torwart("Torwart Team 1");
 
-        team.AddFeldspieler(spieler1);
-        team.AddFeldspieler(spieler2);
-        team.SetTorwart(torwart);
+        team1.AddFeldspieler(spieler1Team1);
+        team1.AddFeldspieler(spieler2Team1);
+        team1.SetTorwart(torwartTeam1);
 
-        int ergebnis = team.Spielzug();
-        Console.WriteLine("Ergebnis des Spielzugs: " + ergebnis);
 
-        spieler1.MacheBlutgraetsche(); //calls method 
-        spieler1.DribbletGegnerSchwindlig();
+        //team 2
+        Feldspieler spieler1Team2 = new Feldspieler("Spieler 1 Team 2");
+        Feldspieler spieler2Team2 = new Feldspieler("Spieler 2 Team 2");
+        Torwart torwartTeam2 =  new Torwart("Torwart Team 2");
 
-        bool schussGehalten = torwart.HalteTor(); //''
-        torwart.Abstoss();
+        team2.AddFeldspieler(spieler1Team2);
+        team2.AddFeldspieler(spieler2Team2);
+        team2.SetTorwart(torwartTeam2);
+
+
+        int ergebnisTeam1 = team1.Spielzug();
+        int ergebnisTeam2 = team2.Spielzug();
+
+
+        Console.WriteLine("Ergebnis des Spielzugs für Team 1: " + ergebnisTeam1);
+        Console.WriteLine("Ergebnis des Spielzugs für Team 2: " + ergebnisTeam2);
+
+        spieler1Team1.MacheBlutgraetsche(); //calls method 
+        torwartTeam1.Abstoss();
+
+        spieler1Team2.DribbletGegnerSchwindlig();
+        bool schussGehalten = torwartTeam2.HalteTor(); 
+   
     }
 }
