6795f7c6007f9443c3738389a9f811762a20c59f
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-10-23 10:35:57 +0200

    Add person, kunde and lagerist in Programm.cs

--- Lidl/Program.cs
+++ Lidl/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var person1 = new Person() {Vorname = "Max", Name = "Mustermann"};
+            var kunde1 = new Kunde() {Vorname = "Fritz", Name = "Müller" };
+            var lagerist1 = new Lagerist() { Vorname = "Hans", Name = "Meier",Groesse = 180 };
+
+            person1.Esse();
+            kunde1.Esse();
+            kunde1.Kaufe();
+            lagerist1.Esse();
+            lagerist1.LagereEin();
         }
     }
 }
