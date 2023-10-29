c3a19cca194e18a0bac763ecbe6e8a851651e9b3
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-10-29 15:02:17 +0100

    Add class 'Square'

---
+++ GeometricForms/Square.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricForms
+{
+    public class Square : Rectangle
+    {
+        public float SideLength
+        {
+            get { return Width; }
+            set
+            {
+                if (value > 0)
+                {
+                    Width = value;
+                    Height = value;
+                }
+                else
+                    throw new ArgumentException("Seitenlänge muss grösser als 0 sein.");
+            }
+        }
+
+        public Square(float x = 100.0f, float y = 100.0f, float sideLength = 1.0f) 
+            : base(x, y, sideLength, sideLength)
+        {
+            SideLength= sideLength;
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine($"Zeichne ein Quadrat mit Seitenlänge {SideLength} an Position ({X}, {Y}).");
+        }
+    }
+}
