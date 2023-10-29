78e7919f1607cb1a74fdbb1361261089c209eaef
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-10-29 13:45:21 +0100

    Add class 'Circle : Shape'

--- GeometricForms/Shape.cs
+++ GeometricForms/Shape.cs
@@ -8,7 +8,7 @@ namespace GeometricForms
         public float Y { get; set; }
         public double RotationAngle { get; set; }
 
-        public Shape(float x, float y)
+        public Shape(float x = 100.0f, float y = 100.0f)
         {
             X = x; 
             Y = y; 
@@ -20,4 +20,33 @@ namespace GeometricForms
         public abstract void Move(float deltaX, float deltaY);
         public abstract void Rotate(double angle);
     }
+
+    public class Circle : Shape
+    {
+        public float Radius { get; set; }
+
+        public Circle(float x = 100.0f, float y = 100.0f, float radius = 1.0f)
+            : base(x, y)
+        {
+            Radius = radius;
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine($"Zeichne einen Kreis mit Radius {Radius} an Position ({X}, {Y}).");
+        }
+
+        public override void Move(float deltaX, float deltaY)
+        {
+            X += deltaX;
+            Y += deltaY;
+            Console.WriteLine($"Verschiebe den Kreis um ({deltaX}, {deltaY}) nach der Position ({X}, {Y}).");
+        }
+        public override void Rotate(double angle)
+        {
+            RotationAngle += angle;
+            Console.WriteLine($"Rotiere den Kreis um {angle} Grad.");
+        }
+    }
 }
+ 
