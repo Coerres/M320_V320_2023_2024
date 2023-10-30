1bf06d925c82851f85da76339279b7e1bb65157e
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-10-30 09:39:17 +0100

    Add project files

---
+++ Komponententest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Komponententest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
