b423f559465692827fcc818fc63e32a34ff127bb
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-11-05 13:53:56 +0100

    Add Abstrakte-Vererbung

--- M320_2023_2024/Program.cs
+++ M320_2023_2024/Program.cs
@@ -97,5 +97,39 @@
             }
         }
 
+
+        public abstract class Shape
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public string Color { get; set; }
+
+            public abstract void Draw();
+        }
+
+
+        //Vererbung (Abstrakt)
+        public class Circle : Shape
+        {
+            public int Radius { get; set; }
+
+            public override void Draw()
+            {
+                // Implementierung zum Zeichnen eines Kreises
+            }
+        }
+
+        public class Rectangle : Shape
+        {
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public override void Draw()
+            {
+                // Implementierung zum Zeichnen eines Rechtecks
+            }
+        }
+
+
     }
 }
