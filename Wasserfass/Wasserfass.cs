9c8233fc8fd05decad5a350984507ca2fd1669db
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-11-06 10:30:19 +0100

    Setup project 'Wasserfass' and add class 'Wasserfass'

---
+++ Wasserfass/Wasserfass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wasserfass
+{
+    public class Wasserfass
+    {
+        public void MaximalPegel()
+        {
+
+        }
+        public void MinimalPegel()
+        {
+
+        }
+    }
+}
