5061e5185f3f434831adfee362f7684b42e128ec
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-11-06 12:00:48 +0100

    Add class 'StringStack'

---
+++ Browser_Simulator/StringStack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Browser_Simulator
+{
+    public class StringStack
+    {
+        string[] values;
+        int index;
+        public StringStack()
+        {
+            values = new string[10];
+        }
+        public void Push(string value)
+        {
+            this.values[index++] = value;
+        }
+        
+        public string Pop()
+        {
+            index--;
+            return this.values[index];
+        }
+    }
+}
