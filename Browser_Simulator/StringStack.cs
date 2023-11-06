2125b86b16e8ce87e149cc00ccadcea50c483f74
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-11-06 12:15:13 +0100

    Change notes

--- Browser_Simulator/StringStack.cs
+++ Browser_Simulator/StringStack.cs
@@ -17,11 +17,46 @@ namespace Browser_Simulator
         public void Push(string value)
         {
             this.values[index++] = value;
+
+            if (index < 0)
+            {
+                Console.WriteLine("Stack ist leer.");
+            }
+            else if (index >= 0 || index <= 8)
+            {
+                Console.WriteLine("Stack ist befüllt.");
+            }
+            else if (index == 9)
+            { 
+ 
+                Console.WriteLine("Stack ist voll.");
+            }
+
         }
-        
+
         public string Pop()
         {
             index--;
+
+            if (index < 0)
+            {
+                Console.WriteLine("Stack ist leer.");
+            }
+            else if (index >= 0 || index <= 8)
+            {
+                Console.WriteLine("Stack ist befüllt.");
+            }
+            else if (index == 9)
+            {
+
+                Console.WriteLine("Stack ist voll.");
+            }
+            return this.values[index];
+
+        }
+
+        public string Peak()
+        {
             return this.values[index];
         }
     }
