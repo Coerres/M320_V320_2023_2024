fdcfebc9828faded18043825c213d19f0cb4a81d
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-11-06 11:41:19 +0100

    Add class 'Simulator'

---
+++ Browser_Simulator/Simulator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Browser_Simulator
+{
+    public class Simulator
+    {
+        public void Run()
+        {
+            while(true)
+            { 
+                Console.Write("Geben Sie eine URL ein:");
+                string url = Console.ReadLine();
+                if (url  == "e")
+                {
+                    break;
+                }
+                BrowseURL(url);
+            }
+        }
+
+        public void BrowseURL(string url)
+        {
+            Console.WriteLine($"Lade {url}");
+        }
+    }
+}
