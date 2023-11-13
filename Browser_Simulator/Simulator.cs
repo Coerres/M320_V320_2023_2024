397ad6b7e4d95a6ecd86a6cbca7c5a0cdbf10f09
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-11-13 08:53:02 +0100

    Add switch-case

--- Browser_Simulator/Simulator.cs
+++ Browser_Simulator/Simulator.cs
@@ -8,17 +8,33 @@ namespace Browser_Simulator
 {
     public class Simulator
     {
+        private StringStack stack = new StringStack();
         public void Run()
         {
             while(true)
             { 
                 Console.Write("Geben Sie eine URL ein:");
                 string url = Console.ReadLine();
+                switch(url)
+                {
+                    case "e":
+                        break;
+                    case "p":
+                        stack.Pop();
+                        break;
+
+                    default:
+                    BrowseURLhere(url);
+                        break;
+
+                }
+                
+                
                 if (url  == "e")
                 {
                     break;
                 }
-                BrowseURLhere(url);
+                
             }
         }
 
