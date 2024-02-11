6ac5286efff6b8c234f6b7192f25327b41225217
Carina Cordes <carina.cordes@ksb-sg.ch>
2024-02-11 12:13:28 +0100

    Add project 'Uebung_Auto'

---
+++ Uebung_Auto_IMS_cc/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Uebung_Auto_IMS_cc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
