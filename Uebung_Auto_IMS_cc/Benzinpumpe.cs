ccfba0628b0734dc772666f1b76b304edfa84f82
Carina Cordes <carina.cordes@ksb-sg.ch>
2024-02-11 12:24:14 +0100

    Add class 'Benzinpumpen'

---
+++ Uebung_Auto_IMS_cc/Benzinpumpe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung_Auto_IMS_cc
+{
+    public class Benzinpumpe
+    {
+        public void Pumpen()
+        {
+            Console.WriteLine("Pumpen");
+        }
+    }
+}
